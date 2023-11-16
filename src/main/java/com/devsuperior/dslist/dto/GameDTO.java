4d16f4d82fc63ad2ff69ab218097e40223aabc5b
davipedro <davi.silva6@estudante.ufla.br>
2023-11-15 21:09:13 -0300

    GET game by ID

--- src/main/java/com/devsuperior/dslist/dto/GameDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -1,22 +1,33 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import org.springframework.beans.BeanUtils;
 
 public class GameDTO {
     private Long id;
     private String title;
     private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
     private String imgUrl;
     private String shortDescription;
+    private String longDescription;
 
-    public GameDTO(){}
+    public GameDTO(){
 
-    public GameDTO(Game entity){
-        id = entity.getId();
-        title = entity.getTitle();
-        year = entity.getYear();
-        imgUrl = entity.getImgUrl();
-        shortDescription = entity.getShortDescription();
+    }
+
+    public GameDTO(Game entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.year = entity.getYear();
+        this.genre = entity.getGenre();
+        this.platforms = entity.getPlatforms();
+        this.score = entity.getScore();
+        this.imgUrl = entity.getImgUrl();
+        this.shortDescription = entity.getShortDescription();
+        this.longDescription = entity.getLongDescription();
     }
 
     public Long getId() {
@@ -31,11 +42,27 @@ public class GameDTO {
         return year;
     }
 
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
     public String getImgUrl() {
         return imgUrl;
     }
 
     public String getShortDescription() {
         return shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
     }
 }
